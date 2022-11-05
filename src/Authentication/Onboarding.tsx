9d4d3a932d10a1ba071cee4eb85916f9e8a05cd1
Ayo <ayoinspired@gmail.com>
2022-11-05 11:45:19 +0100

    Scaffolded onboarding screens

--- src/Authentication/Onboarding.tsx
+++ src/Authentication/Onboarding.tsx
@@ -1,24 +1,39 @@
 import { Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
-import Slide from "../components/Slide";
+import Slide, { SLIDE_HEIGHT } from "../components/Slide";
 
 type Props = {};
 
-const { width } = Dimensions.get("window");
+const { width, height } = Dimensions.get("window");
 
 const Onboarding = (props: Props) => {
 	return (
 		<View style={styles.container}>
 			<View style={styles.slider}>
-				<ScrollView horizontal snapToInterval={width} decelerationRate="fast">
+				<ScrollView
+					horizontal
+					snapToInterval={width}
+					decelerationRate="fast"
+					showsHorizontalScrollIndicator={false}
+					bounces={false}>
 					<Slide label="Relaxed" />
 					<Slide label="Playful" right />
 					<Slide label="Excentric" />
 					<Slide label="Funky" right />
 				</ScrollView>
 			</View>
+			<View style={styles.footer}>
+				<View
+					style={{ ...StyleSheet.absoluteFillObject, backgroundColor: "cyan" }}
+				/>
 
-			<View style={styles.footer}></View>
+				<View
+					style={{
+						flex: 1,
+						backgroundColor: "white",
+						borderTopLeftRadius: 75,
+					}}></View>
+			</View>
 		</View>
 	);
 };
@@ -28,10 +43,13 @@ export default Onboarding;
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		backgroundColor: "white",
 	},
 
 	slider: {
-		flex: 0.61,
+		height: SLIDE_HEIGHT,
+		backgroundColor: "cyan",
+		borderBottomRightRadius: 75,
 	},
 
 	footer: {
--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 type Props = {
@@ -6,14 +6,50 @@ type Props = {
 	right?: boolean;
 };
 
+const { width, height } = Dimensions.get("window");
+
+export const SLIDE_HEIGHT = 0.61 * height;
 const Slide = ({ label, right }: Props) => {
+	const transform = [
+		{ translateY: (SLIDE_HEIGHT - 100) / 2 },
+		{
+			translateX: right ? width / 2 - 50 : -width / 2 + 50,
+		},
+		{ rotate: right ? "-90deg" : "90deg" },
+	];
+
 	return (
-		<View>
-			<Text>{label}</Text>
+		<View style={styles.container}>
+			<View
+				style={[
+					styles.titleContainer,
+					{
+						transform,
+					},
+				]}>
+				<Text style={styles.title}>{label}</Text>
+			</View>
 		</View>
 	);
 };
 
 export default Slide;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		width,
+	},
+
+	title: {
+		fontSize: 80,
+		lineHeight: 80,
+		color: "white",
+		textAlign: "center",
+	},
+
+	titleContainer: {
+		height: 100,
+		justifyContent: "center",
+		transform: [{ translateY: (SLIDE_HEIGHT - 100) / 2 }],
+	},
+});
